1f8f4b7fc30036eda5ef0814031a7b0787e29c0c
Petr Pichugin <petr.pichugin@gmail.com>
2022-11-18 16:39:05 +0500

    layout templates add

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,8 @@ import (
 
 // RenderTemplate renders templates using htlm.Template
 func RenderTemplate(w http.ResponseWriter, templateName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + templateName)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+templateName,
+		"./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template", err)
