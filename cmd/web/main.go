6366204083637b3bec6afa45a015a9087d52d844
Petr Pichugin <petr.pichugin@gmail.com>
2022-11-21 11:43:23 +0500

    add pat http multiplexer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,12 +23,16 @@ func main() {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Println("Starting application on port", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
 }
