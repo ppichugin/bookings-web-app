5f7df83530d84e48282aaa8e99a508bb24a56c07
Petr Pichugin <petr.pichugin@gmail.com>
2022-12-04 15:36:32 +0500

    add post form & csrf token

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
+
 	"github.com/ppichugin/booking-for-breakfast/pkg/config"
 	"github.com/ppichugin/booking-for-breakfast/pkg/models"
 )
@@ -19,13 +21,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
-	// add some data
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html.Template
-func RenderTemplate(w http.ResponseWriter, templateName string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 
 	var tc map[string]*template.Template
 	if app.UseCache {
@@ -36,13 +38,13 @@ func RenderTemplate(w http.ResponseWriter, templateName string, td *models.Templ
 	}
 
 	// get requested template from cache
-	t, ok := tc[templateName]
+	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal("Could not get template from the template cache: ", templateName)
+		log.Fatal("Could not get template from the template cache: ", tmpl)
 	}
 
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	err := t.Execute(buf, td)
 	if err != nil {
 		log.Println(err)
