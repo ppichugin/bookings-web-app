3dc7733d4529b746037cae14c220d8ca0c3acbea
Petr Pichugin <petr.pichugin@gmail.com>
2022-12-09 18:09:25 +0500

    server-side form validation III (min length, place highlight)

--- internal/forms/errors.go
+++ internal/forms/errors.go
@@ -1,14 +1,14 @@
 package forms
 
-type Errors map[string][]string
+type errors map[string][]string
 
 // Add an error message for a given form field
-func (e Errors) Add(field, message string) {
+func (e errors) Add(field, message string) {
 	e[field] = append(e[field], message)
 }
 
 // Get returns the first error message
-func (e Errors) Get(field string) string {
+func (e errors) Get(field string) string {
 	es := e[field]
 	if len(es) == 0 {
 		return ""
--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,14 +1,16 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
-// Form creates a custom form struct, embeds url.Values object
+// Form creates a custom form struct and embeds a url.Values object
 type Form struct {
 	url.Values
-	Errors
+	Errors errors
 }
 
 // New initializes a form structure
@@ -19,11 +21,20 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This is field can not be blank")
 		return false
 	}
 	return true
@@ -32,4 +43,14 @@ func (f *Form) Has(field string, r *http.Request) bool {
 // Valid return true if there are no Errors, otherwise false
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
+}
+
+// MinLength checks for string minimal length
+func (f Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at leats %d characters long", length))
+		return false
+	}
+	return true
 }
